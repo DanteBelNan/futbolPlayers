f3271b1d8ec9768a67cae67951bf378ce8d2e1f5
Dante Beltran <dantebelnan@gmail.com>
2023-04-16 17:49:17 -0300

    card hover created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,16 +15,28 @@
         <div class="card-layout">
         @if(!is_null($players))
             @foreach ($players as $player)
-                <div class="card-layout__item" style="background-image: url({{ asset(($player->avatar ?? 'avatars/default/defaultImage.png')) }})">
+            <div class="card-container">
+            <div class="card-layout__item">
+                <div class="card-layout__header">
                     <h3>{{ $player->name }}</h3>
-                    <p>{{ $player->description }}</p>
+                    <p>Position: {{ $player->posicion1 }}</p>
+                    <p>Skill: {{ $player->skill }}</p>
+                </div>
+                <div class="card-layout__image" style="background-image: url({{ asset(($player->avatar ?? 'avatars/default/defaultImage.png')) }})">
+                </div>
+                <div class="card-layout__footer">
                     <a href="{{ route('players.show', $player->id) }}">View more</a>
                 </div>
+            </div>
+            </div>
+            
             @endforeach
         @else
             <p>No players found.</p>
         @endif
     </main>
 </div>
+
+
 
 @endsection
